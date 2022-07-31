78bf5d9dcc9067ff60b7fc60229ef66a0a2ff764
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-07-31 15:12:43 +0200

    solution

--- code.js
+++ code.js
@@ -3,4 +3,15 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(m, n)
+var uniquePaths = function(m, n) {
+    if (m === 1 || n === 1) {
+        
+        return 1;
+    }
+    
+    else {
+        
+    return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+    
+};
